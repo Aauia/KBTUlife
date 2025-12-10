22e5c9f344d928ced498eee2e874a06f5ca23904
darlxxvii <jekenn9@gmail.com>
2025-12-10 13:47:57 +0500

    created db

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Role
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('outlook', 'first_name', 'last_name', 'is_staff', 'is_superuser')
+    search_fields = ('outlook', 'first_name', 'last_name')
+    ordering = ('outlook',)
+
+    fieldsets = (
+        (None, {'fields': ('outlook', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone', 'avatar_url')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login',)}),
+        ('Role info', {'fields': ('role',)}),
+    )
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
